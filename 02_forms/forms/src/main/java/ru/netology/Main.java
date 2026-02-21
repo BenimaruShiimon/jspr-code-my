f7f895bec81754c9596afd40cff9f5f6ee2f4674
Лефтеров Авраам Андреевич <avraamlefterov@yandex.ru>
2026-02-21 19:17:12 +0300

    "Решение задания №1"

--- 02_forms/forms/src/main/java/ru/netology/Main.java
+++ 02_forms/forms/src/main/java/ru/netology/Main.java
@@ -5,12 +5,13 @@ import ru.netology.server.Server;
 public class Main {
     public static void main(String[] args) {
         Server server = new Server();
-
-        server.addHandler("GET", "/api/hello",
-                (request, responseStream) -> {
-                    responseStream.write(("HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\n" +
+        server.addHandler(
+                "GET",
+                "/api/greeting",
+                (request, response) -> {
+                    response.write(("HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\n" +
                             "Привет, API!").getBytes());
-                    responseStream.flush();
+                response.flush();
                 });
 
         server.start(9999);
